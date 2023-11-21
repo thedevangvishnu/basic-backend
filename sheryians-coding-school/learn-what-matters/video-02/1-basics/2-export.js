7a28577c761c0d8a5234c5fd1d11acabd7f61503
Devang Vishnu <thedevangvishnu@gmail.com>
2023-11-21 16:39:59 +0530

    Learn how to import and export through code in js
    Create two js files and declare some variables. Export variables from
    one file and import into another and use it.

---
+++ sheryians-coding-school/learn-what-matters/video-02/1-basics/2-export.js
@@ -0,0 +1,4 @@
+// Learn how to export in js
+
+let message = "I'm learning NodeJS!";
+module.exports = message;
---
+++ sheryians-coding-school/learn-what-matters/video-02/1-basics/3-import.js
@@ -0,0 +1,7 @@
+// Learn how to import in js
+
+let message = require("./2-export");
+let name = "Devang";
+
+console.log(`Hello everyone! My name is ${name}. ${message}`);
+// Hello everyone! My name is Devang. I'm learning NodeJS.
