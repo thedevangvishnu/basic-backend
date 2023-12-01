725edd9078a8ccca97a9490694cf3a71c748df00
Devang Vishnu <thedevangvishnu@gmail.com>
2023-12-01 10:50:46 +0530

    Learn to create db - start CRUD

--- sheryians-coding-school/the-endgame/video-01/revise-app/routes/index.js
+++ sheryians-coding-school/the-endgame/video-01/revise-app/routes/index.js
@@ -1,5 +1,3 @@
-var express = require("express");
-var router = express.Router();
 /* 
 Before express generator, we used to do this
 var express = require("express");
@@ -11,9 +9,24 @@ instead of app.get() we now use router.get()
 everything else remains as usual. 
 */
 
+const express = require("express");
+const router = express.Router();
+const userModel = require("./users");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+// Basic CRUD
+// Create
+router.get("/create", async function (req, res, next) {
+  const user = await userModel.create({
+    username: "Devang",
+    age: 24,
+    email: "dv080499@gmail.com",
+  });
+  res.send(user);
 });
 
 module.exports = router;
